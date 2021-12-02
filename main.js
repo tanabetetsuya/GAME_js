d1a0a612ef86cce48f8d8b5d7011ec8c47fcb11a
tanabetetsuya <81751235+tanabetetsuya@users.noreply.github.com>
2021-12-02 21:53:39 +0900

    [Add] 星の移動の処理の追加

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+//ゲームスピード
+const GAME_SPEED = 1000/60;
+
 //画面サイズ
 const SCREEN_W = 180;
 const SCREEN_H = 320;
@@ -51,7 +54,15 @@ class Star{
 let star=[];
 for(let i=0; i<STAR_MAX; i++)star[i] = new Star();
 
-con.fillStyle= "black";
-con.fillRect(0,0,SCREEN_W, SCREEN_H);
 
-for(let i=0; i<STAR_MAX; i++)star[i].draw();
+setInterval( gameLoop, GAME_SPEED );
+
+function gameLoop()
+{
+   //移動の処理
+   for(let i=0;i<STAR_MAX; i++)star[i].update();
+   //描画の処理
+   con.fillStyle= "black";
+   con.fillRect(0,0,SCREEN_W, SCREEN_H);
+   for(let i=0; i<STAR_MAX; i++)star[i].draw();
+}
